4311a08f1a34d29573d9e33570b0ad0c584857a2
Lucas Sinke <lsinke@student.codam.nl>
2021-12-18 17:56:33 +0100

    Add codam header to every single file

--- ft_printf_internal.h
+++ ft_printf_internal.h
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_printf_internal.h                               :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lsinke <lsinke@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/12/18 17:55:07 by lsinke        #+#    #+#                 */
+/*   Updated: 2021/12/18 17:55:07 by lsinke        ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
 #ifndef FT_PRINTF_INTERNAL_H
 # define FT_PRINTF_INTERNAL_H
 # include <stdarg.h>
